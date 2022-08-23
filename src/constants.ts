ac83e474e084104b5dc3481f01c4ec7d815c1730
nam.dohoang <nam.dohoang@vti.com.vn>
2022-08-23 17:22:26 +0700

    comapre data API-030

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,5 @@ export enum ENDPOINTS {
   GET_PROJECTS_AFFILIATE_CONVERSIONS_APPROVED = 'get-projects-affiliate-projectId-conversions-approved',
   GET_PROJECTS_AFFILIATE_CONVERSIONS = 'get-projects-affiliate-conversions',
   GET_INFLUENCER_AFFILIATE_SUMMARY = 'get-influencer-affiliate-influencerId-summary',
+  GET_INFLUENCER_INFLUENCERID_EVALUATIONS = 'get-influencer-influencerId-evaluations',
 }
