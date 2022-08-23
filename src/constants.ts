417dda636a2188fc12dc83290865b4aa367f2a1d
VTI\son.ledang <son.ledang>
2022-08-23 16:11:35 +0700

    create source testcase 005

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,5 @@ export enum ENDPOINTS {
   GET_PROJECTS_AFFILIATE_CONVERSIONS_UNAPPROVED = 'get-projects-affiliate-conversions-unapproved',
   GET_PROJECTS_AFFILIATE_CONVERSIONS = 'get-projects-affiliate-conversions',
   GET_INFLUENCER_AFFILIATE_SUMMARY = 'get-influencer-affiliate-influencerId-summary',
+  GET_AFFILIATE_PROJECTID_CONVERSION_DETAIL_CONVERIONID = 'get-affiliate-projectid-conversions-detail-conversionid',
 }
